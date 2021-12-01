4e4558d20e92779563e0cac055e5f6bf0e85f728
planchon <paul@planchon.io>
2021-12-01 20:12:58 +0100

    merging des deux parties

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 import os
 from flask import Flask, jsonify, request, session, Response, send_from_directory
 from flask_cors import CORS
+from sentence_transformers import SentenceTransformer
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv("./covid_19faq.csv")
+model = SentenceTransformer("bert-base-nli-mean-tokens")
+question_bert_vector = np.load(open("./question_faq.pickle", "rb"))
 
 app = Flask(__name__)
 CORS(app)
 
+def get_response(question):
+    test = model.encode([question])
+    i = np.argmax(cosine_similarity(test, question_bert_vector))
+    return df["answers"].iloc[i]
+    
 @app.route('/', defaults=dict(filename=None))
 @app.route('/<path:filename>', methods=['GET'])
 def index(filename):
@@ -20,7 +34,7 @@ def ask():
     data = request.json["message"]
     print("question :", data)
     return jsonify({
-        "message": "c paul"
+        "message": get_response(data)
     })
 
 if __name__ == "__main__":
